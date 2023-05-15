5c282950f37163d3ecdaf761c1f8f28675c31a38
eat <934567800@qq.com>
2023-05-15 16:51:00 +0800

    第一次提交

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: "./",
+  devServer: {
+    // 跨域代理
+    proxy: {
+      [process.env.VUE_APP_BASE_API]: {
+        target: process.env.VUE_APP_SERVICE_URL,
+        changeOrigin: true,
+        pathRewrite: {
+          ["^" + process.env.VUE_APP_BASE_API]: "",
+        },
+      },
+    },
+  },
 });
